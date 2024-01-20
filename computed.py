3e564e5d0c478fb0e59502da0be00ded903119f4
Will Abbott <will@digitaloutback.co.uk>
2024-01-20 21:21:12 +0000

    first

---
+++ computed.py
@@ -0,0 +1,71 @@
+import unittest
+import ast
+import inspect
+import textwrap
+
+
+class ComputedProperty:
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        if instance not in self.cache:
+            self.cache[instance] = self.func(instance)
+        return self.cache[instance]
+
+    def invalidate(self, instance):
+        if instance in self.cache:
+            del self.cache[instance]
+
+
+class DependencyExtractor(ast.NodeVisitor):
+    def __init__(self):
+        self.dependencies = set()
+
+    def visit_Attribute(self, node):
+        if isinstance(node.value, ast.Name) and node.value.id == 'self':
+            self.dependencies.add(node.attr)
+        self.generic_visit(node)
+
+
+def extract_dependencies(func):
+    # get method source code
+    source = inspect.getsource(func)
+
+    # dedent source code because we're taking this method outside of the class and parsing it
+    source = textwrap.dedent(source)
+
+    tree = ast.parse(source)
+    extractor = DependencyExtractor()
+    extractor.visit(tree)
+    return extractor.dependencies
+
+
+class ComputedPropertyMeta(type):
+    def __new__(cls, name, bases, dct):
+        computed_properties = []
+        for attr_name, attr_value in dct.items():
+            if isinstance(attr_value, ComputedProperty):
+                computed_properties.append(attr_value)
+                attr_value.func.dependencies = extract_dependencies(attr_value.func)
+        dct['_computed_properties'] = computed_properties
+        return super().__new__(cls, name, bases, dct)
+
+
+class HasComputedProperties(metaclass=ComputedPropertyMeta):
+    def __init__(self):
+        super().__init__()
+
+    def sync_input(self, name, value):
+        super().__setattr__(name, value)
+        # Invalidate cached computed properties if their dependencies are changed
+        for prop in self._computed_properties:
+            if name in prop.func.dependencies:
+                prop.invalidate(self)
+
+
+def computed_property(func):
+    return ComputedProperty(func)
---
+++ computed_tests.py
@@ -0,0 +1,52 @@
+import unittest
+
+from computed import HasComputedProperties, computed_property
+
+
+# Example class using the system for testing
+class Example(HasComputedProperties):
+    def __init__(self, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.fake_operation_count = 0
+
+    def fake_compute_operation(self):
+        self.fake_operation_count += 1
+
+    @computed_property
+    def my_computed(self):
+        self.fake_compute_operation()
+        return self.x * self.y
+
+
+class TestComputedProperty(unittest.TestCase):
+    def test_computed_property_evaluation(self):
+        example = Example(5, 3)
+        self.assertEqual(example.my_computed, 15)
+
+    def test_computed_property_caching(self):
+        example = Example(5, 3)
+        _ = example.my_computed  # This should use the cache
+        _ = example.my_computed  # This should use the cache
+        _ = example.my_computed  # This should use the cache
+        self.assertEqual(example.fake_operation_count, 1)
+
+    def test_invalidation_on_dependency_change(self):
+        example = Example(5, 3)
+        self.assertEqual(example.fake_operation_count, 0)
+        _ = example.my_computed  # Initial calculation
+        self.assertEqual(example.fake_operation_count, 1)
+        example.sync_input('x', 10)  # Change a dependency
+        self.assertEqual(example.my_computed, 30)  # Recalculation should happen
+        self.assertEqual(example.fake_operation_count, 2)
+
+    def test_no_invalidation_on_non_dependency_change(self):
+        example = Example(5, 3)
+        first_result = example.my_computed  # Initial calculation
+        example.z = 100  # Change a non-dependency
+        self.assertEqual(example.my_computed, first_result)  # Should use cached value
+
+
+if __name__ == '__main__':
+    unittest.main()
